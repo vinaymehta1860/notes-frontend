9b27a3a7a418970f72195201854d8641215843a4
Vinay Mehta <vini@Vinays-MacBook-Air.local>
2018-12-27 00:38:39 -0700

    Pushing the initial setup for react app to the repo.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>This is the new beginning.</h1>
+        <h2>It's only a matter of time before your life comes through for you..!!</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
