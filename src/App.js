e8e768cb5e0adc327ff3ead93b9ccac02aaf80f9
Vinay Mehta <vini@Vinays-MacBook-Air.local>
2019-01-01 17:57:20 -0700

    Defining all the routes in a different file for modularity.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import './App.css';
-import LandingPage from './components/LandingPage';
-import Registration from './components/Registration';
-import HomePage from './components/home/HomePage';
+import Routes from './Routes';
 
 class App extends React.Component {
   render() {
     return (
       <Router>
         <div className="app">
-          <Route path="/" exact component={LandingPage} />
-          
-          <Route path="/signin" 
-            render={(props) => <Registration {...props} action={"signin"} val="randomValueToCheck" />}
-          />
-          
-          <Route path="/signup" 
-            render={(props) => <Registration {...props} action={"signup"} val="randomValueToCheck" />}
-          />
-
-          <Route path='/homepage'
-            render={(props) => <HomePage {...props} />}
-          />
+          <Routes />
         </div>
       </Router>
     );
